054a245c8ba88c1f22887785e987752e7f5d98ea
UrkoLekuona <urkole@hotmail.com>
2019-08-08 14:30:05 +0200

    Fixed post install script

--- post_install.sh
+++ post_install.sh
@@ -7,20 +7,21 @@
 LOG=/root/postinstall.log
 
 # Updates and new packages
-apt-get update 2> $LOG
-apt-get upgrade -y 2> $LOG
-apt-get install -y vim dconf-cli 2> $LOG
-apt-get autoremove 2> $LOG
+apt-get update 2>> $LOG
+apt-get upgrade -y 2>> $LOG
+apt-get install -y vim dconf-cli 2>> $LOG
+apt-get autoremove 2>> $LOG
 
 # Locale
-dconf reset -f /org/gnome/terminal 2> $LOG
-apt-get remove gnome-terminal -y 2> $LOG
-apt-get install gnome-terminal -y 2> $LOG
-locale-gen --purge en_US.UTF-8 2> $LOG
-update-locale 2> $LOG
+dconf reset -f /org/gnome/terminal 2>> $LOG
+apt-get remove gnome-terminal -y 2>> $LOG
+apt-get install gnome-terminal -y 2>> $LOG
+locale-gen --purge en_US.UTF-8 2>> $LOG
+dpkg-reconfigure locales --default en_US.UTF-8 2>> $LOG
+update-locale 2>> $LOG
 
 # DHCP interfaces
-sed -i 's/manual/dhcp/g' /etc/network/interfaces 2> $LOG
+sed -i 's/manual/dhcp/g' /etc/network/interfaces 2>> $LOG
 
 # Testing if script has been executed
 if [ ! -f $LOG ]; then
